e6ee5df7d362769f2a3ef8b415db432d998109b9
Christian Krivickis <S00198911@mail.itsligo.ie>
2021-03-26 10:27:24 +0000

    Code done, error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { OmdbApiService } from "./services/omdb-api.service";
+import { IOMDResponse } from "./omdbresponse";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [OmdbApiService]
 })
+
 export class AppComponent {
-  title = 'movie-finder';
+  
+  movieData:IOMDResponse;
+  errorMessage:any;
+
+  constructor(private _ombdbService:OmdbApiService) {}
+
+  getMovieDetails(movieName:string) : boolean {
+    this._ombdbService.getMovieData(movieName).subscribe(
+      movieData => {
+        this.movieData=movieData;
+        console.log("Director Name: " + this.movieData.Director);
+      },
+      error => this.errorMessage = <any>error
+    );
+    return false;
+  }
 }
--- src/app/services/omdb-api.service.ts
+++ src/app/services/omdb-api.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { catchError, tap } from "rxjs/operators";
+import { IOMDResponse } from "../omdbresponse";
 
 @Injectable({
   providedIn: 'root'
 })
-export class OmdbApiService {
 
-  constructor() { }
+export class OmdbApiService {
+  private _siteURL="https://www.ombdapi.com/";
+  private _key="?apikey=12a6fa17&t=";
+  private handleError(err:HttpErrorResponse) {
+    console.log("OmdbApiService: " + err.message);
+    return Observable.throw(err.message);
+  }
+
+  constructor(private _http:HttpClient) { }
+
+  getMovieData(movieName) : Observable<IOMDResponse> {
+    return this._http.get<IOMDResponse>(this._siteURL + this._key + movieName)
+    .pipe(
+      tap(data => console.log("Moviedata/error" + JSON.stringify(data))
+    ),
+      catchError(this.handleError)
+    );
+  }
 }
